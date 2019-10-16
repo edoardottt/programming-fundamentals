8d787ca11fa51b1b5394576d6de74b453e20e789
gilfoyle97 <edoardott@gmail.com>
2019-10-16 18:29:22 +0200

    Update exercise6.py

--- Exercises/exercise6.py
+++ Exercises/exercise6.py
@@ -4,6 +4,8 @@
 Created on Wed Oct  9 16:05:27 2019
 
 @author: edoardottt
+https://www.edoardoottavianelli.it
+https://www.github.com/edoardottt
 """
 '''
 This function prints the sum of the numbers in a list.

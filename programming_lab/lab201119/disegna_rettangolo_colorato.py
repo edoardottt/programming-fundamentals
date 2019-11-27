11266828a47fb8ae2f98f731bf125f7b284faa4b
gilfoyle97 <edoardott@gmail.com>
2019-11-27 11:48:44 +0100

    Add files via upload

---
+++ programming_lab/lab201119/disegna_rettangolo_colorato.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 17:38:49 2019
+
+@author: angelo
+Scrivere una funzione disegna_rettangolo_colorato(imm, x1, y1, x2, y2, colore_bordo, colore_interno) che prende
+
+    una immagine imm come lista di liste di tuple di colore
+    due punti x1,y1 e x2,y2 corrispondenti a due spigoli di un rettangolo
+    due tuple di colori colore_bordo e colore_interno
+    e modifica la immagine imm disegnando il rettangolo individuato dai due punti x1,y1 e x2,y2, usando colore_bordo per il perimetro del rettangolo e colore_interno per l'area. La funzione ritorna il numero di pixel modificati in imm.
+
+    """
+def disegna_rettangolo_colorato(imm, x1, y1, x2, y2, colore_bordo, colore_interno):
+    imm[y1][x1:x2] = [colore_bordo] * (x2-x1)
+    imm[y2][x1:x2] = [colore_bordo] * (x2-x1)
+    for riga in range(y1+1, y2):
+        imm[riga][x1:x2] = [colore_bordo] + [colore_interno]* (x2-x1-2) + [colore_bordo]
+
+
+
---
+++ programming_lab/lab201119/strisce.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 16:05:46 2019
+
+@author: angelo
+"""
+
+mango = (255,119,51)
+nero = (0,0,0)
+bianco = (255,255,255)
+rosso = (255,0,0)
+verde = (0,255,0)
+blu = (0,0,255)
+ultra = (255,102,255)
+
+from immagini import load, save
+
+def strisce(imm, col1, col2):
+    """ Riempie l'immagine imm con strisce di colori alternati col1, col2 """
+    for i in range(0,len(imm),2):
+        imm[i] = [col1] * len(imm[0])
+        imm[i+1] = [col2] * len(imm[0])
+    if len(imm) % 2 == 1:
+        imm[-1] = [col1] * len(imm[0])
+
+def strisce_ver(imm, col1, col2):
+    for i in range(len(imm)):
+        for j in range(len(imm[0])):
+            if j % 2 == 0:
+                imm[i][j] = col1
+            else:
+                imm[i][j] = col2
+
+def strisce_ver2(imm, col1, col2):
+    for i in range(len(imm)):
+        imm[i][:len(imm[0])] = [col1, col2] * (len(imm[0])//2)
+    if len(imm[0]) % 2 == 1:
+        for i in range(len(imm)):
+            imm[i][-1]=col1
+
+
+

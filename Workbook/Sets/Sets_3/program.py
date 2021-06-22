404b2f8d035b53e73598a854a1e9a3db219d66b6
edoardottt <edoardott@gmail.com>
2021-06-22 21:08:42 +0200

    update

--- Workbook/Sets/Sets_3/program.py
+++ Workbook/Sets/Sets_3/program.py
@@ -1,5 +1,5 @@
 def es3(ins1, ins2):
-    '''
+    """
     progettare la funzione es3(ins1, ins2) che:
     - riceve  in input due insiemi  di numeri naturali
     - trova le terne (a,b,c) con a,b e c in insi1 con la proprieta' che a<b<c e a+b+c e' in insi2
@@ -10,20 +10,21 @@ def es3(ins1, ins2):
     ESEMPIO:
     se ins1={ 2,4,5,6,8,9} e ins2={5,15,19,25} la funzione restituisce la lista
     [(2, 4, 9), (2, 5, 8), (4, 5, 6), (2, 8, 9), (4, 6, 9), (5, 6, 8)]
-    '''
+    """
     insieme = set()
     for a in ins1:
         for b in ins1:
             for c in ins1:
-                if a!=b!=c:
-                    if a<b<c and a+b+c in ins2:
-                        insieme.add((a,b,c))
+                if a != b != c:
+                    if a < b < c and a + b + c in ins2:
+                        insieme.add((a, b, c))
     diz = {}
     for elem in insieme:
-        if sum(elem) not in diz: diz[sum(elem)] = [elem]
-        else: diz[sum(elem)].append(elem)
+        if sum(elem) not in diz:
+            diz[sum(elem)] = [elem]
+        else:
+            diz[sum(elem)].append(elem)
     result = []
     for k in sorted(diz.keys()):
         result.extend(sorted(diz[k]))
     return result
-        
--- Workbook/Sets/Sets_5/program.py
+++ Workbook/Sets/Sets_5/program.py
@@ -1,23 +1,25 @@
 def es44(a, b):
-    '''
-    Si progetti la funzione es44(a,b) che,  in input due interi a e b restituisce 
+    """
+    Si progetti la funzione es44(a,b) che,  in input due interi a e b restituisce
     l'insieme dei primi a interi che hanno esattamente b divisori.
     Ad esempio per a=20 e b=2 la funzione deve restituire l'insieme dei primi 20 numeri primi
     (perche' solo i numeri primi hanno esattamente 2 divisori), vale a dire:
     {2, 3, 67, 5, 37, 7, 71, 41, 11, 43, 13, 47, 17, 19, 61, 53, 23, 59, 29, 31}
-    '''
+    """
     goon = True
     insieme = set()
     i = 2
     while goon:
         divisori = set()
-        for j in range(1,i+1):
-            if i%j==0:
+        for j in range(1, i + 1):
+            if i % j == 0:
                 divisori.add(j)
-        if len(divisori)==b:
+        if len(divisori) == b:
             insieme.add(i)
-        i+=1
-        if len(insieme)==a: goon=False
+        i += 1
+        if len(insieme) == a:
+            goon = False
     return insieme
 
-print(es44(2,4))
+
+print(es44(2, 4))

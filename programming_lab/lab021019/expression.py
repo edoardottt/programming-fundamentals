ef1b5eb4e5c44a8f901e5bc5975330d6a8e74a46
pavan kumar molleti <48610563+POP1912@users.noreply.github.com>
2021-06-23 16:00:35 +0530

    Update and rename espressione.py to expression.py

---
+++ programming_lab/lab021019/expression.py
@@ -0,0 +1,6 @@
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+
+result = a ** 2 + (b ** 3 * c ** 4) / (3 * a) - b + a * c
+print("The result of the expression is", result)

79cb77f8d98540731265f01ce5b8b89da237e009
pavan kumar molleti <48610563+POP1912@users.noreply.github.com>
2021-06-23 20:06:43 +0530

    Update factorial_ric.py

--- programming_lab/lab041219/factorial_ric.py
+++ programming_lab/lab041219/factorial_ric.py
@@ -5,11 +5,11 @@ Created on Wed Dec  4 14:55:46 2019
 
 @author: angelo
 
-Scrivere la funzione factorial ricorsiva.
-Ricordarsi che n! = n * (n-1)!
+Write the recursive factorial function.
+Remember that n! = n * (n-1)!
 
-caso_base:     n=1 -> 1
-caso_ricorsivo: n>1 -> n * (n-1)!
+base_case:     n=1 -> 1
+recursive_case: n>1 -> n * (n-1)!
 """
 
 

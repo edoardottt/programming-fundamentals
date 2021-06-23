fd7abde1fe23533ce09d739a85d3f54ef6e8e059
pavan kumar molleti <48610563+POP1912@users.noreply.github.com>
2021-06-23 15:54:22 +0530

    Update equazione_secondo_grado.py

--- programming_lab/lab021019/equazione_secondo_grado.py
+++ programming_lab/lab021019/equazione_secondo_grado.py
@@ -1,8 +1,8 @@
-a = float(input("Dammi a: "))
-b = float(input("Dammi b: "))
-c = float(input("Dammi c: "))
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("enter c: "))
 delta = b ** 2 - 4 * a * c
 x1 = (-b + delta ** 0.5) / (2 * a)
 x2 = (-b - delta ** 0.5) / (2 * a)
-print("La soluzione positiva dell'equazione {}x^2+{}x+{}=0 e'".format(a, b, c), x1)
-print("La soluzione negativa dell'equazione {}x^2+{}x+{}=0 e'".format(a, b, c), x2)
+print("The positive solution of the equation {}x^2+{}x+{}=0 is".format(a, b, c), x1)
+print("The negative solution of the equation {}x^2+{}x+{}=0 is".format(a, b, c), x2)

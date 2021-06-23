70a8da040e71d3f72b85a73baecff89c52fb21a6
pavan kumar molleti <48610563+POP1912@users.noreply.github.com>
2021-06-23 15:53:00 +0530

    Update anonimom3.py
    Correct the Output from "positive" to "negative" in line 6

--- programming_lab/lab021019/anonimom3.py
+++ programming_lab/lab021019/anonimom3.py
@@ -2,8 +2,8 @@ def foo(a, b, c):
     delta = b ** 2 - 4 * a * c
     x1 = (-b + delta ** 0.5) / (2 * a)
     x2 = (-b - delta ** 0.5) / (2 * a)
-    print("la soluzione positiva dell'equazione {}x^2+{}x+{}=0 è:".format(a, b, c), x1)
-    print("la soluzione positiva dell'equazione {}x^2+{}x+{}=0 è:".format(a, b, c), x2)
+    print("the positive solution of the equation {}x^2+{}x+{}=0 is:".format(a, b, c), x1)
+    print("the negative solution of the equation {}x^2+{}x+{}=0 is:".format(a, b, c), x2)
     return x1, x2
 
 

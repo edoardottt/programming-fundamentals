ab4b779c190bdbd9c4590adab20b56caec813ab1
pavan kumar molleti <48610563+POP1912@users.noreply.github.com>
2021-06-23 15:26:20 +0530

    Update anonimo2.py

--- programming_lab/lab021019/anonimo2.py
+++ programming_lab/lab021019/anonimo2.py
@@ -1,15 +1,15 @@
-def equazione_positiva(a, b, c):
+def positive_equation(a, b, c):
      x1=(-b+(b**2-4*a*c)**0.5)/(2*a)
      return(x1)
-def equazione_negativa(a,b,c):
+def negative_equation(a,b,c):
      x2 = (-b-(b**2-4*a*c)**0.5)/(2*a)
      return x2
 
 
 
-# (-9+0.8569568250501305j) ma perchè mi da sto risultato?
+# (-9+0.8569568250501305j) but why do you give me this result?
 
-​def equazione(a,b,c):
+​def equation(a,b,c):
   radpos = ((-b)+(sqrt(b**2-4*a*c)))/2*a
  ​ radneg= ((-b)-(sqrt(b**2-4*a*c)))/2*a
   ​return radpos, radneg

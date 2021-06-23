f878c3e14f0aebd628e6f55fdd31c36da07a756f
pavan kumar molleti <48610563+POP1912@users.noreply.github.com>
2021-06-23 15:37:57 +0530

    Update testa_anonimo.py

--- programming_lab/lab021019/testa_anonimo.py
+++ programming_lab/lab021019/testa_anonimo.py
@@ -1,17 +1,17 @@
 import anonimo2
 
-x1 = anonimo2.equazione_positiva(1, 2, 1)
-x2 = anonimo2.equazione_negativa(1, 2, 1)
+x1 = anonimo2.positive_equation(1, 2, 1)
+x2 = anonimo2.negative_equation(1, 2, 1)
 
-print("Se il tuo codice è corretto, x1 deve valere", -1)
+print("If your code is correct, x1 must be valid", -1)
 print(x1)
-print("Se il tuo codice è corretto, x2 deve valere", 1)
+print("If your code is correct, x2 must be valid", 1)
 print(x2)
 
-x1 = anonimo2.equazione_positiva(3, -5, -2)
-x2 = anonimo2.equazione_negativa(3, -5, -2)
+x1 = anonimo2.positive_equation(3, -5, -2)
+x2 = anonimo2.negative_equation(3, -5, -2)
 
-print("Se il tuo codice è corretto, x1 deve valere", 2)
+print("If your code is correct, x1 must be valid", 2)
 print(x1)
-print("Se il tuo codice è corretto, x2 deve valere", 1 / 3)
+print("If your code is correct, x2 must be valid", 1 / 3)
 print(x2)

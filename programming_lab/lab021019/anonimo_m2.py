054c9032b4c312e19ecab2a10832f40520553c87
pavan kumar molleti <48610563+POP1912@users.noreply.github.com>
2021-06-23 15:35:25 +0530

    Update anonimo_m2.py

--- programming_lab/lab021019/anonimo_m2.py
+++ programming_lab/lab021019/anonimo_m2.py
@@ -1,18 +1,18 @@
-# Il programma non entra nelle due funzioni definite
-a = float(input("Inserisci a "))
-b = float(input("Inserisci b "))
-c = float(input("Inserisci c "))
+# The program does not enter the two defined functions
+a = float(input("insert a "))
+b = float(input("insert b "))
+c = float(input("insert c "))
 
-# non entra nella funzione
+# does not enter the function
 def x1(a, b, c):
     delta = (b ** 2 - 4 * a * c) ** (1 / 2)
     xx1 = (-b + delta) / (2 * a)
-    print("La soluzione positiva è", xx1)
+    print("The positive solution is", xx1)
     return xx1
 
 
 def x2(a, b, c):
     delta = (b ** 2 - 4 * a * c) ** (1 / 2)
     xx2 = (-b - delta) / (2 * a)
-    print("La soluzione negativa è", xx2)
+    print("The negative solution is", xx2)
     return xx2

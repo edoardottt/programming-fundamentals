307cd7bccbe257a274421d7397eaaebed3eb2b9a
pavan kumar molleti <48610563+POP1912@users.noreply.github.com>
2021-06-27 21:06:52 +0530

    Rename point.py to punto.py

--- programming_lab/lab271119/punto.py
+++ programming_lab/lab271119/punto.py
@@ -7,78 +7,78 @@ Created on Wed Nov 27 09:18:12 2019
 """
 
 
-class Punto:
-    """un punto bidimensionale"""
+class Point:
+    """A two-dimensional point"""
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def __str__(self):
-        return "Punto({},{})".format(self.x, self.y)
+        return "Point({},{})".format(self.x, self.y)
 
     def __repr__(self):
-        return "Punto(x={},y={})".format(self.x, self.y)
+        return "Point(x={},y={})".format(self.x, self.y)
 
-    def __sub__(self, altro_punto):
-        return self.distanza(altro_punto)
+    def __sub__(self, other_Point):
+        return self.distance(other_Point)
 
-    def distanza(self, altro_punto):
-        if self.x == altro_punto.x:
-            d = abs(self.y - altro_punto.y)
-        elif self.y == altro_punto.y:
-            d = abs(self.x - altro_punto.x)
+    def distance(self, other_Point):
+        if self.x == other_Point.x:
+            d = abs(self.y - other_Point.y)
+        elif self.y == other_Point.y:
+            d = abs(self.x - other_Point.x)
         else:
-            d = ((self.x - altro_punto.x) ** 2 + (self.y - altro_punto.y) ** 2) ** 0.5
+            d = ((self.x - other_Point.x) ** 2 + (self.y - other_Point.y) ** 2) ** 0.5
         return d
 
 
-class Quadrato:
-    """un quadrato nel piano cartesiano"""
+class Square:
+    """A square in the Cartesian plane"""
 
-    def __init__(self, p, lato):
-        #        if not isinstance(p, Punto):
-        if type(p) != Punto:
-            raise TypeError("l'oggetto Quadrato ha bisogno di un punto di origine")
-        if not isinstance(lato, (int, float)):
-            raise TypeError("l'oggetto Quadrato ha bisogno di un lato di tipo numerico")
-        if lato == 0:
-            raise ValueError("il lato del Quadrato non può essere 0")
-        if lato < 0:
-            lato = -lato
-            p = Punto(p.x - lato, p.y - lato)
-        self.vertice_as = Punto(p.x, p.y)
-        self.vertice_ad = Punto(p.x + lato, p.y)
-        self.vertice_bs = Punto(p.x, p.y + lato)
-        self.vertice_bd = Punto(p.x + lato, p.y + lato)
+    def __init__(self, p, side):
+        #        if not isinstance(p, Point):
+        if type(p) != Point:
+            raise TypeError("The Square object needs an origin point")
+        if not isinstance(side, (int, float)):
+            raise TypeError("The Square object needs a side of the numeric type")
+        if side == 0:
+            raise ValueError("The side of the Square cannot be 0")
+        if side < 0:
+            side = -side
+            p = Point(p.x - side, p.y - side)
+        self.vertice_as = Point(p.x, p.y)
+        self.vertice_ad = Point(p.x + side, p.y)
+        self.vertice_bs = Point(p.x, p.y + side)
+        self.vertice_bd = Point(p.x + side, p.y + side)
 
-    def perimetro(self):
-        """ritorna il perimetro del quadrato"""
-        return self.vertice_ad.distanza(self.vertice_as) * 4
+    def perimeter(self):
+        """Returns the perimeter of the square"""
+        return self.vertice_ad.distance(self.vertice_as) * 4
 
     def area(self):
-        """ritorna l'area del quadrato"""
-        return self.vertice_ad.distanza(self.vertice_as) ** 2
+        """Returns the area of the square"""
+        return self.vertice_ad.distance(self.vertice_as) ** 2
 
 
-class Rettangolo:
-    """un rettangolo nel piano cartesiano"""
+class Rectangle:
+    """A rectangle in the Cartesian plane"""
 
     def __init__(self, p1, p2):
-        if type(p1) != Punto or type(p2) != Punto:
-            raise TypeError("Il rettangolo ha due Punti come vertici")
+        if type(p1) != Point or type(p2) != Point:
+            raise TypeError("The rectangle has two points as vertices")
         if p1.x == p2.x or p1.y == p2.y:
-            raise ValueError("Il rettangolo ha una dimensione nulla")
-        self.vertice_as = Punto(min(p1.x, p2.x), min(p1.y, p2.y))
-        self.vertice_bd = Punto(max(p1.x, p2.x), max(p1.y, p2.y))
-        self.vertice_ad = Punto(self.vertice_bd.x, self.vertice_as.y)
-        self.vertice_bs = Punto(self.vertice_as.x, self.vertice_bd.y)
+            raise ValueError("The rectangle has a zero size")
+        self.vertice_as = Point(min(p1.x, p2.x), min(p1.y, p2.y))
+        self.vertice_bd = Point(max(p1.x, p2.x), max(p1.y, p2.y))
+        self.vertice_ad = Point(self.vertice_bd.x, self.vertice_as.y)
+        self.vertice_bs = Point(self.vertice_as.x, self.vertice_bd.y)
 
         self.base = self.vertice_bd - self.vertice_bs
-        self.altezza = self.vertice_bs - self.vertice_as
+        self.height = self.vertice_bs - self.vertice_as
 
-    def perimetro(self):
-        return self.base * 2 + self.altezza * 2
+    def perimeter(self):
+        return self.base * 2 + self.height * 2
 
     def area(self):
-        return self.base * self.altezza
+        return self.base * self.height

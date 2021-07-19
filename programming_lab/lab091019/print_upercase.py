72c8732d5491390f53634ba91b7e783f881430b1
pavan kumar molleti <48610563+POP1912@users.noreply.github.com>
2021-07-19 19:25:12 +0530

    Update and rename stampa_maiuscolo.py to print_upercase.py

---
+++ programming_lab/lab091019/print_upercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 16:23:21 2019
+
+@author: angelo
+"""
+
+
+def print_upercase(list):
+    """It takes a list of strings and prints each element of the list uppercase, one per line """
+    for element in list:
+        print(element.upper())

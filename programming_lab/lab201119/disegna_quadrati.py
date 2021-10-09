e695b1fd6b99217bf43cd52ca28e4d1a7a48d6bd
pavan kumar molleti <48610563+POP1912@users.noreply.github.com>
2021-10-09 08:53:19 +0530

    Update disegna_quadrati.py

--- programming_lab/lab201119/disegna_quadrati.py
+++ programming_lab/lab201119/disegna_quadrati.py
@@ -4,56 +4,53 @@
 Created on Wed Nov 20 11:31:37 2019
 
 @author: angelo
-Scrivere una funzione disegna_quadrato_colorato(imm, x1, y1, x2, y2, colore_bordo, colore_interno) che prende
+Write a draw_color_square function (pic, x1, y1, x2, y2, border_color, inner_color) that takes
 
-    una immagine imm come lista di liste di tuple di colore
-    due punti x1,y1 e x2,y2 corrispondenti a due spigoli di un rettangolo
-    due tuple di colori colore_bordo e colore_interno
-    e modifica la immagine imm disegnando il rettangolo individuato dai due punti x1,y1 e x2,y2, usando colore_bordo per il perimetro del rettangolo e colore_interno per l'area. La funzione ritorna il numero di pixel modificati in imm.
+    an image pic as a list of color tuple lists 10 two points x1, y1 and x2, y2 corresponding to two edges of a rectangle 11 two color tuples border_color and inner_color 12 and modify the image im by drawing the rectangle identified by the two points x1, y1 and x2, y2, using color_border for the perimeter of the rectangle and color_in for the area. The function returns the number of pixels modified in images.
 """
 
-from immagini import load, save
+from images import load, save
 
-nero = (0,0,0)
-bianco = (255,255,255)
-rosso = (255,0,0)
-verde = (0,255,0)
-blu = (0,0,255)
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
 amazon = (59,122,87)
 
-imm = []
-altezza = 50
-larghezza = 120
-for i in range(altezza):
-    riga = [nero] * larghezza
-    imm.append(riga)
+pic = []
+height = 50
+length = 120
+for i in range(height):
+    row = [black] * length
+    pic.append(row)
 
-save(imm, 'rettangolo_50_120.png')
+save(pic, 'rectangle_50_120.png')
 
-for i in range(len(imm[0])):
+for i in range(len(pic[0])):
     for j in range(5):
-        imm[j][i]=rosso
-        imm[-j-1][i]=rosso
+        pic[j][i]=red
+        pic[-j-1][i]=red
 
-for i in range(len(imm)):
+for i in range(len(pic)):
     for j in range(5):
-        imm[i][j]=rosso
-        imm[i][-j-1]=rosso
+        pic[i][j]=red
+        pic[i][-j-1]=red
 
-save(imm, 'rettangolo_50_120_constriscia.png')
+save(pic, 'rectangle_50_120_construct.png')
 
-def disegna_quadrato_colorato(imm, x1, y1, x2, y2, colore_bordo, colore_interno):
+def draw_colorful_square(pic, x1, y1, x2, y2, border_color, interior_color):
     if x1 > x2:
         x1, x2 = x2, x1
     if y1 > y2:
         y1, y2 = y2, y1
-    for riga in range(y1+1, y2):
-        imm[riga][x1] = colore_bordo
-        imm[riga][x2] = colore_bordo
+    for row in range(y1+1, y2):
+        pic[row][x1] = border_color
+        pic[row][x2] = border_color
         for pixel in range(x1+1, x2):
-            imm[riga][pixel] = colore_interno
-    for riga in [y1, y2]:
+            pic[row][pixel] = interior_color
+    for row in [y1, y2]:
         for pixel in range(x1, x2+1):
-            imm[riga][pixel] = colore_bordo
+            pic[row][pixel] = border_color
 
 

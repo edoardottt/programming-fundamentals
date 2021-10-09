790a71bf1168f775c3503a3280f036fd1c67c8a9
pavan kumar molleti <48610563+POP1912@users.noreply.github.com>
2021-10-09 09:34:44 +0530

    Update strisce.py

--- programming_lab/lab201119/strisce.py
+++ programming_lab/lab201119/strisce.py
@@ -7,37 +7,37 @@ Created on Wed Nov 20 16:05:46 2019
 """
 
 mango = (255, 119, 51)
-nero = (0, 0, 0)
-bianco = (255, 255, 255)
-rosso = (255, 0, 0)
-verde = (0, 255, 0)
-blu = (0, 0, 255)
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
 ultra = (255, 102, 255)
 
-from immagini import load, save
+from images import load, save
 
 
-def strisce(imm, col1, col2):
-    """Riempie l'immagine imm con strisce di colori alternati col1, col2"""
-    for i in range(0, len(imm), 2):
-        imm[i] = [col1] * len(imm[0])
-        imm[i + 1] = [col2] * len(imm[0])
-    if len(imm) % 2 == 1:
-        imm[-1] = [col1] * len(imm[0])
+def stripes(pic, col1, col2):
+    """Fills the pic image with streaks of alternating colors col1, col2"""
+    for i in range(0, len(pic), 2):
+        pic[i] = [col1] * len(pic[0])
+        pic[i + 1] = [col2] * len(pic[0])
+    if len(pic) % 2 == 1:
+        pic[-1] = [col1] * len(pic[0])
 
 
-def strisce_ver(imm, col1, col2):
-    for i in range(len(imm)):
-        for j in range(len(imm[0])):
+def stripes_ver(pic, col1, col2):
+    for i in range(len(pic)):
+        for j in range(len(pic[0])):
             if j % 2 == 0:
-                imm[i][j] = col1
+                pic[i][j] = col1
             else:
-                imm[i][j] = col2
+                pic[i][j] = col2
 
 
-def strisce_ver2(imm, col1, col2):
-    for i in range(len(imm)):
-        imm[i][: len(imm[0])] = [col1, col2] * (len(imm[0]) // 2)
-    if len(imm[0]) % 2 == 1:
-        for i in range(len(imm)):
-            imm[i][-1] = col1
+def stripes_ver2(pic, col1, col2):
+    for i in range(len(pic)):
+        pic[i][: len(pic[0])] = [col1, col2] * (len(pic[0]) // 2)
+    if len(pic[0]) % 2 == 1:
+        for i in range(len(pic)):
+            pic[i][-1] = col1

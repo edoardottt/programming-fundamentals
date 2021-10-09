d6e43fbd8e56993268f41d7f5876feef5760bd75
pavan kumar molleti <48610563+POP1912@users.noreply.github.com>
2021-10-09 08:15:04 +0530

    Update and rename crea_matrici.py to create_matrices.py

---
+++ programming_lab/lab201119/create_matrices.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 14:10:59 2019
+
+@author: angelo
+
+"""
+
+
+def create_matrix_numbers(n, m):
+    """the function constructs a matrix that has all numbers from 1 to n * m ordered by line 13 e.g.:
+        create_matrix_numbers(2, 4) return
+        [[1,2,3,4],[5,6,7,8]]"""
+
+    matrix = []
+    row = []
+    for i in range(1, n * m + 1):
+        row.append(i)
+        if i % m == 0:
+            matrix.append(row)
+            row = []
+    return matrix
+
+
+def create_array_numbers2(n, m):
+    matrix = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(i * m + j + 1)
+        matrix.append(row)
+    return matrix
+
+
+def create_array_numbers3(n, m):
+    matrix = []
+    for i in range(n):
+        row = []
+        for j in range(i * m + 1, i * m + m + 1):
+            row.append(j)
+        matrix.append(row)
+    return matrix
+
+
+def create_array_numbers4(n, m):
+    return [[j for j in range(i * m + 1, i * m + m + 1)] for i in range(n)]

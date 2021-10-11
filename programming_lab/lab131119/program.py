b64c64ee87b7b50a10b966ebe4eb43f81014c3d9
pavan kumar molleti <48610563+POP1912@users.noreply.github.com>
2021-10-11 18:33:51 +0530

    Update program.py

--- programming_lab/lab131119/program.py
+++ programming_lab/lab131119/program.py
@@ -1,11 +1,11 @@
-# somma di matrici:
+#sum of matrices
 #
 # 1 2 3    2 2 2   3  4  5
 # 4 5 6 +  2 2 2 = 6  7  8
 # 7 8 9    2 2 2   9 10 11
 
 
-def somma(mat1, mat2):
+def sum(mat1, mat2):
     mat3 = mat1.copy()
     for i in range(len(mat1)):
         for j in range(len(mat1[0])):
@@ -15,27 +15,27 @@ def somma(mat1, mat2):
 
 mat1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 mat2 = [[2, 2, 2], [2, 2, 2], [2, 2, 2]]
-somma(mat1, mat2)
+sum(mat1, mat2)
 
 
 ################################################
 
-# trasposta di matrice:
+# matrix transpose:
 #
-# da matrice h x w si ottiene matrice w x h
+# from matrix h x w we obtain matrix w x h
 # 1 2 3            1 4
 # 4 5 6  ------>   2 5
 #                  3 6
 
 
-def trasposta(mat):
+def transpose(mat):
     h, w = len(mat), len(mat[0])
-    # crea  matrice di zeri di dimensioni w x h
+    # creates matrix of zeros of dimensions w x h
     mat1 = []
     for h1 in range(w):
-        riga = [0] * h
-        mat1.append(riga)
-    # inserisce i valori nella matrice
+        row = [0] * h
+        mat1.append(row)
+    # inserts values into the matrix
     for i in range(h):
         for j in range(w):
             mat1[j][i] = mat[i][j]
@@ -43,14 +43,14 @@ def trasposta(mat):
 
 
 mat = [[1, 2, 3], [4, 5, 6]]
-trasposta(mat)
+transpose(mat)
 #  [[1, 4], [2, 5], [3, 6]]
 
 
 ###############################################
 
-# prodotto di matrici
-# matrici di dimensioni 3x2 e 2x4 danno matrice 3x4
+# product of matrices
+# matrices of dimensions 3x2 and 2x4 give matrix 3x4
 #
 # 1 2    1 2 3 4      11 14 17 20
 # 3 4 *  5 6 7 8  =   23 30 37 44
@@ -68,12 +68,12 @@ def prodScal(i, j, mat1, mat2):
 def prod(mat1, mat2):
     h1 = len(mat1)
     w2 = len(mat2[0])
-    # genera matrice di zeri di dimensioni h1xw2
+    # generates matrix of zeros of size h1xw2
     mat3 = []
     for i in range(h1):
         riga = [0] * w2
         mat3.append(riga)
-    # inserisce i valori nella matrice
+    # inserts values into the array
     for i in range(h1):
         for j in range(w2):
             mat3[i][j] = prodScal(i, j, mat1, mat2)
@@ -90,102 +90,104 @@ prod(mat1, mat2)
 ###############################################
 
 
-def crea(h, w, c=(0, 0, 0)):
-    """ritorna un'immagine monocromatica  di altezza
-    alt e larghezza lar e colore c"""
+def create(h, w, c=(0, 0, 0)):
+    """returns a monochrome height image 
+    alt and width lar and color c"""
     img = []
     for _ in range(h):
-        riga = [c] * w
-        img += [riga]
+        row = [c] * w
+        img += [row]
     return img
 
 
-a = crea(10, 10)
+a = create(10, 10)
 
-import immagini
+import images
 
-immagini.save(a, "prova1.png")
+images.save(a, "prova1.png")
 
 ################################################
 
 
-def ruota1(img):
-    """Ritorna una nuova immagine che e' l'immagine
-    img ruotata intorno al suo asse orizzontale
+def rotate1(img):
+    """
+    Returns a new image which is the image 
+    img rotated around its horizontal axis
     """
     h, w = len(img), len(img[0])
-    ret = crea(h, w, (0, 0, 0))
+    ret = create(h, w, (0, 0, 0))
     for y in range(h):
         for x in range(w):
             ret[y][x] = img[h - 1 - y][x]
     return ret
 
 
-img = immagini.load("fiore.png")
-img1 = ruota1(img)
-immagini.save(img1, "fiore1.png")
+img = images.load("fiore.png")
+img1 = rotate1(img)
+images.save(img1, "fiore1.png")
 
 
 ###############################################
 
 
-def ruota2(img):
-    """Ritorna una nuova immagine che e' l'immagine
-    in cui ho scambiato righe con colonne
+def rotate2(img):
+    """
+    Returns a new image which is the image 
+    where I swapped rows with columns
     """
     h, w = len(img), len(img[0])
-    ret = crea(w, h, (0, 0, 0))
+    ret = create(w, h, (0, 0, 0))
     for y in range(h):
         for x in range(w):
             ret[x][y] = img[y][x]
     return ret
 
 
-img = immagini.load("fiore.png")
-img1 = ruota2(img)
-immagini.save(img1, "fiore1.png")
+img = images.load("fiore.png")
+img1 = rotate2(img)
+images.save(img1, "fiore1.png")
 ##################################################
 
 
-def bordo(img, s, c):
-    """restituisce immagine img con  bordo di spessore s  e colore c"""
+def border(img, s, c):
+    """returns img image with border of thickness s and color c"""
     w, h = len(img[0]), len(img)
-    img1 = crea(h + s * 2, w + s * 2, c)
-    for riga in range(h):
-        for colonna in range(w):
-            img1[riga + s][colonna + s] = img[riga][colonna]
+    img1 = create(h + s * 2, w + s * 2, c)
+    for row in range(h):
+        for column in range(w):
+            img1[row + s][column + s] = img[row][column]
     return img1
 
 
-img = immagini.load("fiore.png")
-img1 = bordo(img, 10, (255, 0, 0))
-immagini.save(img1, "fiore1.png")
+img = images.load("fiore.png")
+img1 = border(img, 10, (255, 0, 0))
+images.save(img1, "fiore1.png")
 
 #################################################
 
 
-def quadrato(img, y, x, l, col):
-    """inserisce su img un quadrato di lato l con vertice in
-    alto a sinistra nel pixel i,j"""
+def square(img, y, x, l, col):
+    """inserts on img a square of side l with vertex in 
+    top left in pixel i, j"""
     for i in range(y, y + l):
         for j in range(x, x + l):
             img[i][j] = col
 
 
-img = crea(100, 100)
-quadrato(img, 10, 10, 30, (255, 0, 0))
-quadrato(img, 30, 20, 50, (0, 255, 0))
-immagini.save(img, "prova.png")
+img = create(100, 100)
+square(img, 10, 10, 30, (255, 0, 0))
+square(img, 30, 20, 50, (0, 255, 0))
+images.save(img, "prova.png")
 
 
-quadrato(img, 70, 70, 50, (0, 0, 255))
+square(img, 70, 70, 50, (0, 0, 255))
 
-# versione corretta
+# correct version
 
 
-def quadrato1(img, y, x, l, col):
-    """inserisce su img un quadrato di lato l con vertice in
-    alto a sinistra nel pixel i,j"""
+def square1(img, y, x, l, col):
+    """inserts on img a square of side l with vertex in 
+    top left in pixel i, j"""
     h, w = len(img), len(img[0])
     for i in range(y, y + l):
         for j in range(x, x + l):
@@ -193,17 +195,17 @@ def quadrato1(img, y, x, l, col):
                 img[i][j] = col
 
 
-quadrato1(img, 70, 70, 50, (0, 0, 255))
-immagini.save(img, "prova.png")
+square1(img, 70, 70, 50, (0, 0, 255))
+images.save(img, "prova.png")
 
 ##################################################
 
 
-def inverti(img):
-    """Ritorna una nuova immagine che e'
-    l'immagine img con colori invertiti"""
+def invert(img):
+    """Returns a new image that is 
+    img image with inverted colors"""
     w, h = len(img[0]), len(img)
-    ret = crea(h, w)
+    ret = create(h, w)
     for y in range(h):
         for x in range(w):
             r, g, b = img[y][x]
@@ -211,20 +213,22 @@ def inverti(img):
     return ret
 
 
-img = immagini.load("fiore.png")
-img1 = inverti(img)
-immagini.save(img1, "fiore1.png")
+img = images.load("fiore.png")
+img1 = invert(img)
+images.save(img1, "fiore1.png")
 
 ####################################################
 
 
-def grigio(img):
-    """ritorna una nuova immagine che e' l'immagine
-    img con i colori in scala di grigi.
-    In questo caso ciascun pixel di colori R,G,B deve avere come
-    colore la loro media"""
+def grey(img):
+    """
+    returns a new image which is the image 
+    img with grayscale colors. 
+    In this case each pixel of colors R, G, B must have as 
+    color their average
+    """
     h, w = len(img), len(img[0])
-    img1 = crea(h, w)
+    img1 = create(h, w)
     for y in range(h):
         for x in range(w):
             c = round(sum(img[y][x]) / 3)
@@ -232,6 +236,6 @@ def grigio(img):
     return img1
 
 
-img = immagini.load("fiore.png")
-img1 = grigio(img)
-immagini.save(img1, "fiore1.png")
+img = images.load("fiore.png")
+img1 = grey(img)
+images.save(img1, "fiore1.png")
